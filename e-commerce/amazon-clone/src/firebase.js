1f6281cb6eefc300aa717c5a094fb374441bcd8a
HappyCodingMama <happycodingmama@gmail.com>
2020-11-02 21:52:23 +0900

    implement react

---
+++ e-commerce/amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+      apiKey: "AIzaSyDak99t6sfRxgqp9UB_PHUTsbNoooX4PKw",
+      authDomain: "clone-379c9.firebaseapp.com",
+      databaseURL: "https://clone-379c9.firebaseio.com",
+      projectId: "clone-379c9",
+      storageBucket: "clone-379c9.appspot.com",
+      messagingSenderId: "340721896715",
+      appId: "1:340721896715:web:bb689280a61c790381660e",
+      measurementId: "G-WKRPFWGSHS"
+};
