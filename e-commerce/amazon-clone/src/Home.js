288e89bba8213909e1d8c078f77332f6cc5e29ce
HappyCodingMama <happycodingmama@gmail.com>
2020-11-06 23:52:18 +0900

    build the home component

--- e-commerce/amazon-clone/src/Home.js
+++ e-commerce/amazon-clone/src/Home.js
@@ -4,7 +4,12 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <p>I am the Home component</p>
+      <div className="home__container">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Sports_en_US_1x._CB431860448_.jpg"
+          alt="home"
+        />
+      </div>
     </div>
   );
 }
