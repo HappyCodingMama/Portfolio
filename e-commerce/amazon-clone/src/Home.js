d64d17d8cc5c3f8e9e0c0a36f20042b6311ff950
HappyCodingMama <happycodingmama@gmail.com>
2020-11-07 20:58:01 +0900

    build the product component

--- e-commerce/amazon-clone/src/Home.js
+++ e-commerce/amazon-clone/src/Home.js
@@ -6,9 +6,25 @@ function Home() {
     <div className="home">
       <div className="home__container">
         <img
+        className="home__image"
           src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Sports_en_US_1x._CB431860448_.jpg"
-          alt="home"
+          alt="home image"
         />
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className="home__row">
+          {/* Product */}
+        </div>
+
       </div>
     </div>
   );
