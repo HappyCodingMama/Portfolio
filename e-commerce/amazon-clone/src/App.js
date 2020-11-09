d2bafed911ea934185e9cb91671afbf66610338a
HappyCodingMama <happycodingmama@gmail.com>
2020-11-09 22:00:36 +0900

    build the checkout page

--- e-commerce/amazon-clone/src/App.js
+++ e-commerce/amazon-clone/src/App.js
@@ -10,13 +10,13 @@ function App() {
     // BEM
     <Router>
       <div className="app">
+        <Header />
+
         <Switch>
           <Route path="/checkout">
-            <Header />
             <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
--- e-commerce/amazon-clone/src/Checkout.js
+++ e-commerce/amazon-clone/src/Checkout.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./Checkout.css";
 
 function Checkout() {
-  return <div className="checkout"></div>;
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/gift-certificates/consumer/2020/bday/bday.jpg"
+          alt=""
+        />
+
+        <div>
+          <h2 className="checkout__title">Your shopping Cart</h2>
+        </div>
+      </div>
+
+      <div className="checkout__right">
+        <h2>The subtotal will go here</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
