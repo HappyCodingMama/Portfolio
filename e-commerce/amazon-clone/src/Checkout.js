a77f6ddaa32897c011cf8e5cb600f63b7b384017
HappyCodingMama <happycodingmama@gmail.com>
2020-11-11 20:57:25 +0900

    build the subtotal section

--- e-commerce/amazon-clone/src/Checkout.js
+++ e-commerce/amazon-clone/src/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -14,19 +15,16 @@ function Checkout() {
         <div>
           <h2 className="checkout__title">Your shopping Cart</h2>
 
-          
-        {/* BasketItem */}
-        {/* BasketItem */}
-        {/* BasketItem */}
-        {/* BasketItem */}
-        {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
+          {/* BasketItem */}
         </div>
       </div>
 
       <div className="checkout__right">
         <Subtotal />
-        <h2>The subtotal will go here</h2>
-
       </div>
     </div>
   );
