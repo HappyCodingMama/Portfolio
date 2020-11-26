4c24866225f9c6d978f8eb352bc58fbd67a66b68
HappyCodingMama <happycodingmama@gmail.com>
2020-11-26 23:13:09 +0900

    connect the database

--- e-commerce/amazon-clone/src/App.js
+++ e-commerce/amazon-clone/src/App.js
@@ -6,6 +6,7 @@ import Home from "./Home";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Orders from "./Orders";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import { loadStripe } from "@stripe/stripe-js";
@@ -45,6 +46,10 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
